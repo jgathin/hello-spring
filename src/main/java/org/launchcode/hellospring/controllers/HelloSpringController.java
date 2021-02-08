49dd637333b072755718f422394b32992153e789
Jeremy Gathing <bigguy@Jeremys-Mac-mini.local>
2021-02-08 15:08:56 -0600

    Finished exercise 10.5 No bonus

--- src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
@@ -4,13 +4,34 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+
 public class HelloSpringController {
 
     @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
-    public String hello(@RequestParam String coder){
-        return "What's up, " + coder + "!\n" + "Welcome to the beautiful world of coding!!\n"
-                + "We hope that you have fun!!";
+    public String createMessage(@RequestParam String coder, String language){
+
+
+        switch (language){
+            case "english":
+                return "Hello, " + coder;
+
+            case "french":
+                return "Bonjour, " + coder;
+
+            case "spanish":
+                return "Hola, " + coder;
+
+            case "german":
+            case "dutch":
+                return "Hallo, " + coder;
+            case "portuguese":
+                return "Ola, " + coder;
+            case "italian":
+                return "Ciao, " + coder;
+
+        }
+    return language;
     }
 
     @GetMapping
@@ -19,12 +40,27 @@ public class HelloSpringController {
         String html =
                 "<html>" +
                         "<body>" +
-                        "<form method = 'post' action = '/hello'>" +
+                        "<form method = 'post' action = '/hello' id = 'helloform'>" +
                         "<input type = 'text' name = 'coder' />" +
+                        "<select id = 'languages' name = 'language' form = 'helloform' />"+
+                        "<option value = 'english'>English</option>" +
+                        "<option value = 'french'>French</option>" +
+                        "<option value = 'spanish'>Spanish</option>" +
+                        "<option value = 'german'>German</option>" +
+                        "<option value = 'dutch'>Dutch</option>" +
+                        "<option value = 'portuguese'>Portuguese</option>" +
+                        "<option value = 'italian'>Italian</option>" +
+                        "</select>" +
                         "<input type = 'submit' value = 'Greet Me!' />" +
                         "</form>" +
                         "</body" +
                         "</html>";
         return html;
     }
+
+//    @GetMapping("goodbye")
+//    @ResponseBody
+//    public String helloGoodbye(){
+//        return "Hello, Goodbye my niggas!!";
+//    }
 }
